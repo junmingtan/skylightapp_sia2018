542515c9f1b37a902871c9594bc1f90e9d1aaf29
Lim Xiao Qi <xqlim1996@gmail.com>
2018-10-21 01:24:11 +0800

    test2

--- app/src/main/java/me/eugenekoh/skylightapp/Chat.java
+++ app/src/main/java/me/eugenekoh/skylightapp/Chat.java
@@ -1,7 +1,6 @@
 package me.eugenekoh.skylightapp;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.PorterDuff;
 import android.location.Location;
@@ -10,7 +9,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -43,7 +41,6 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.ibm.watson.developer_cloud.conversation.v1.Conversation;
-import com.ibm.watson.developer_cloud.conversation.v1.model.Context;
 import com.ibm.watson.developer_cloud.conversation.v1.model.InputData;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
@@ -70,7 +67,6 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import android.location.LocationListener;
-import me.eugenekoh.skylightapp.utils.GPSTracker;
 //import android.content.Context;
 //firestore
 //chatbot
@@ -160,30 +156,34 @@ public class Chat extends AppCompatActivity implements LocationListener{
 
     private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.grey_60), PorterDuff.Mode.SRC_ATOP);
         setSupportActionBar(toolbar);
         actionBar = getSupportActionBar();
         actionBar.setTitle("Chat");
-        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
         Tools.setSystemBarColor(this, R.color.grey_20);
     }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_logout, menu);
-        Tools.changeMenuIconColor(menu, getResources().getColor(R.color.grey_60));
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_logout) {
-            Toast.makeText(getApplicationContext(), "Logging out...", Toast.LENGTH_SHORT).show();
-            SharedPreferences sp = getSharedPreferences("login",MODE_PRIVATE);
-            sp.edit().putBoolean("logged", false).apply();
-            startActivity(new Intent(Chat.this, LoginCardLight.class));
+        if (item.getItemId() == android.R.id.home) {
             finish();
+        } else {
+            Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
     }
+
 
     //GPS
 
@@ -704,25 +704,44 @@ public class Chat extends AppCompatActivity implements LocationListener{
 
 
     private void initComponent() {
-        BottomNavigationView navi = findViewById(R.id.navigation);
-        navi.setVisibility(View.VISIBLE);
-        navi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        relativeLayout = (RelativeLayout) findViewById(R.id.chatlayout);
+        tab_layout = (TabLayout) findViewById(R.id.tab_layout);
+
+        tab_layout .addTab(tab_layout.newTab().setIcon(R.drawable.ic_chat), 0);
+        tab_layout.addTab(tab_layout.newTab().setIcon(R.drawable.ic_timeline), 1);
+
+        // set icon color pre-selected
+        tab_layout.getTabAt(0).getIcon().setColorFilter(getResources().getColor(R.color.deep_orange_500), PorterDuff.Mode.SRC_IN);
+        tab_layout.getTabAt(1).getIcon().setColorFilter(getResources().getColor(R.color.grey_60), PorterDuff.Mode.SRC_IN);
+
+        tab_layout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.navigation_chat:
-                        return true;
-                    case R.id.navigation_flight:
-                        startActivity(new Intent(Chat.this, Flights.class));
-                        finish();
-                        return true;
-                    case R.id.navigation_travel:
+            public void onTabSelected(TabLayout.Tab tab) {
+                tab.getIcon().setColorFilter(getResources().getColor(R.color.deep_orange_500), PorterDuff.Mode.SRC_IN);
+                switch (tab.getPosition()) {
+                    case 0:
+                        break;
+                    case 1:
                         startActivity(new Intent(Chat.this, Travel.class));
                         finish();
-                        return true;
+                        break;
                 }
-                return false;
+
+                ViewAnimation.fadeOutIn(relativeLayout);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tab.getIcon().setColorFilter(getResources().getColor(R.color.grey_60), PorterDuff.Mode.SRC_IN);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
+
+        Tools.setSystemBarColor(this, R.color.grey_5);
+        Tools.setSystemBarLight(this);
     }
 }
---
+++ app/src/main/java/me/eugenekoh/skylightapp/Constants.java
@@ -0,0 +1,8 @@
+package me.eugenekoh.skylightapp;
+
+public class Constants {
+
+    public static final String CHANNEL_ID = "my_channel_01";
+    public static final String CHANNEL_NAME = "SkylightApp";
+    public static final String CHANNEL_DESCRIPTION = "SkylightApp";
+}
