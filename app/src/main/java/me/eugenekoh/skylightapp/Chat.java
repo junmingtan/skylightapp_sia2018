64c875dc5a28eee5d204bc95b95060f6b4b63252
Eugene Koh <eucjerie@gmail.com>
2018-10-21 01:20:21 +0800

    Completed

--- app/src/main/java/me/eugenekoh/skylightapp/Chat.java
+++ app/src/main/java/me/eugenekoh/skylightapp/Chat.java
@@ -1,6 +1,7 @@
 package me.eugenekoh.skylightapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.PorterDuff;
 import android.location.Location;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -41,6 +43,7 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.ibm.watson.developer_cloud.conversation.v1.Conversation;
+import com.ibm.watson.developer_cloud.conversation.v1.model.Context;
 import com.ibm.watson.developer_cloud.conversation.v1.model.InputData;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
@@ -67,6 +70,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import android.location.LocationListener;
+import me.eugenekoh.skylightapp.utils.GPSTracker;
 //import android.content.Context;
 //firestore
 //chatbot
@@ -156,34 +160,30 @@ public class Chat extends AppCompatActivity implements LocationListener{
 
     private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_menu);
-        toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.grey_60), PorterDuff.Mode.SRC_ATOP);
         setSupportActionBar(toolbar);
         actionBar = getSupportActionBar();
         actionBar.setTitle("Chat");
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);
         Tools.setSystemBarColor(this, R.color.grey_20);
     }
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_logout, menu);
+        Tools.changeMenuIconColor(menu, getResources().getColor(R.color.grey_60));
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
+        if (item.getItemId() == R.id.action_logout) {
+            Toast.makeText(getApplicationContext(), "Logging out...", Toast.LENGTH_SHORT).show();
+            SharedPreferences sp = getSharedPreferences("login",MODE_PRIVATE);
+            sp.edit().putBoolean("logged", false).apply();
+            startActivity(new Intent(Chat.this, LoginCardLight.class));
             finish();
-        } else {
-            Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
     }
-
 
     //GPS
 
@@ -704,44 +704,25 @@ public class Chat extends AppCompatActivity implements LocationListener{
 
 
     private void initComponent() {
-        relativeLayout = (RelativeLayout) findViewById(R.id.chatlayout);
-        tab_layout = (TabLayout) findViewById(R.id.tab_layout);
-
-        tab_layout .addTab(tab_layout.newTab().setIcon(R.drawable.ic_chat), 0);
-        tab_layout.addTab(tab_layout.newTab().setIcon(R.drawable.ic_timeline), 1);
-
-        // set icon color pre-selected
-        tab_layout.getTabAt(0).getIcon().setColorFilter(getResources().getColor(R.color.deep_orange_500), PorterDuff.Mode.SRC_IN);
-        tab_layout.getTabAt(1).getIcon().setColorFilter(getResources().getColor(R.color.grey_60), PorterDuff.Mode.SRC_IN);
-
-        tab_layout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        BottomNavigationView navi = findViewById(R.id.navigation);
+        navi.setVisibility(View.VISIBLE);
+        navi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                tab.getIcon().setColorFilter(getResources().getColor(R.color.deep_orange_500), PorterDuff.Mode.SRC_IN);
-                switch (tab.getPosition()) {
-                    case 0:
-                        break;
-                    case 1:
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_chat:
+                        return true;
+                    case R.id.navigation_flight:
+                        startActivity(new Intent(Chat.this, Flights.class));
+                        finish();
+                        return true;
+                    case R.id.navigation_travel:
                         startActivity(new Intent(Chat.this, Travel.class));
                         finish();
-                        break;
+                        return true;
                 }
-
-                ViewAnimation.fadeOutIn(relativeLayout);
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                tab.getIcon().setColorFilter(getResources().getColor(R.color.grey_60), PorterDuff.Mode.SRC_IN);
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
+                return false;
             }
         });
-
-        Tools.setSystemBarColor(this, R.color.grey_5);
-        Tools.setSystemBarLight(this);
     }
 }
--- app/src/main/java/me/eugenekoh/skylightapp/notifications/MyFirebaseMessagingService.java
+++ app/src/main/java/me/eugenekoh/skylightapp/notifications/MyFirebaseMessagingService.java
@@ -1,31 +1,14 @@
-/**
- * Copyright 2016 Google Inc. All Rights Reserved.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package me.eugenekoh.skylightapp.notifications;
 
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -33,103 +16,32 @@ import com.google.firebase.messaging.RemoteMessage;
 import me.eugenekoh.skylightapp.Flights;
 import me.eugenekoh.skylightapp.R;
 
-
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-
-    private static final String TAG = "XiaoQi";
-    private static final int BROADCAST_NOTIFICATION_ID = 1;
-
-    @Override
-    public void onDeletedMessages() {
-        super.onDeletedMessages();
-    }
-
-    /**
-     * Called when message is received.
-     *
-     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
-     */
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-
-        String notificationBody = "";
-        String notificationTitle = "";
-        String notificationData = "";
-        try{
-            notificationData = remoteMessage.getData().toString();
-            notificationTitle = remoteMessage.getNotification().getTitle();
-            notificationBody = remoteMessage.getNotification().getBody();
-        }catch (NullPointerException e){
-            Log.e(TAG, "onMessageReceived: NullPointerException: " + e.getMessage() );
-        }
-        Log.d(TAG, "onMessageReceived: data: " + notificationData);
-        Log.d(TAG, "onMessageReceived: notification body: " + notificationBody);
-        Log.d(TAG, "onMessageReceived: notification title: " + notificationTitle);
-
+        super.onMessageReceived(remoteMessage);
+        sendNotification(remoteMessage.getNotification().getBody());
     }
 
-    /**
-     * Build a push notification for a chat message
-     * @param title
-     * @param message
-     */
-    private void sendMessageNotification(String title, String message, String messageId){
-        Log.d(TAG, "sendChatmessageNotification: building a chatmessage notification");
+    private void sendNotification(String messageBody){
+        Intent intent = new Intent(this, Flights.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        //get the notification id
-        int notificationId = buildNotificationId(messageId);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 ,intent, PendingIntent.FLAG_ONE_SHOT);
 
-        // Instantiate a Builder object.
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,
-                getString(R.string.default_notification_channel_id));
-        // Creates an Intent for the Activity
-        Intent pendingIntent = new Intent(this, Flights.class);
-        // Sets the Activity to start in a new, empty task
-        pendingIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        // Creates the PendingIntent
-        PendingIntent notifyPendingIntent =
-                PendingIntent.getActivity(
-                        this,
-                        0,
-                        pendingIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-        builder.setContentIntent(notifyPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this );
+        notificationBuilder.setSmallIcon(R.drawable.ic_flight);
+        notificationBuilder.setContentTitle("Skylight");
+        notificationBuilder.setContentTitle(messageBody);
+        notificationBuilder.setAutoCancel(true);
+        notificationBuilder.setSound(defaultSoundUri);
+        notificationBuilder.setContentIntent(pendingIntent);
 
-        mNotificationManager.notify(notificationId, builder.build());
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notificationBuilder.build());
+
 
     }
-
-
-    private int buildNotificationId(String id){
-        Log.d(TAG, "buildNotificationId: building a notification id.");
-
-        int notificationId = 0;
-        for(int i = 0; i < 9; i++){
-            notificationId = notificationId + id.charAt(0);
-        }
-        Log.d(TAG, "buildNotificationId: id: " + id);
-        Log.d(TAG, "buildNotificationId: notification id:" + notificationId);
-        return notificationId;
-    }
-
-
-    /**
-     * Called if InstanceID token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the InstanceID token
-     * is initially generated so this is where you would retrieve the token.
-     */
-    @Override
-    public void onNewToken(String token) {
-        Log.d(TAG, "Refreshed token: " + token);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-        //sendRegistrationToServer(token);
-    }
-
 }
